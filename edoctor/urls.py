9cf5b108d381c63f75557a73b49fe5e2b59dc685
walimike <walimike139@gmail>
2019-10-15 13:06:35 +0300

    link UI to home view function

--- edoctor/urls.py
+++ edoctor/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', home_view)
 ]
