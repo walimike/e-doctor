751e57480bb4b54ac145905822bf7e31502e254a
walimike <walimike139@gmail>
2019-10-15 15:01:41 +0300

    add inline validation for form inputs

--- UI/statics/js/home.js
+++ UI/statics/js/home.js
@@ -2,6 +2,18 @@ let submitForm = () =>{
     alert("Submit")
 }
 
-function myScript () {
-    alert("Submit")
+let validatePassword = () =>{
+    password = document.getElementById("password")
+    confirm_password = document.getElementById("re_enter_password")
+    if (password.value != confirm_password.value) {
+        document.getElementById('message').style.color = 'red';
+        document.getElementById('message').innerHTML = 'not matching';
+    }else{
+        document.getElementById('message').style.color = 'green';
+        document.getElementById('message').innerHTML = 'matching';
+    }
 }
+
+let emailIsValid = (email) =>{
+    return /\S+@\S+\.\S+/.test(email)
+  }
